fbd78823cd8937cbfa062b3e68fde30f06bb84cc
Amir Mofasser <amir.mofasser@volvocars.com>
2017-01-21 12:29:48 +0100

    Added buildconstraint for color constants on windows platforms. Added ability to enable/disable timestamp, levelname and logger name.

---
+++ color.go
@@ -0,0 +1,15 @@
+// +build !windows
+
+package loglevel
+
+const (
+	CLR_0 = "\x1b[30;1m"
+	CLR_R = "\x1b[31;1m"
+	CLR_G = "\x1b[32;1m"
+	CLR_Y = "\x1b[33;1m"
+	CLR_B = "\x1b[34;1m"
+	CLR_M = "\x1b[35;1m"
+	CLR_C = "\x1b[36;1m"
+	CLR_W = "\x1b[37;1m"
+	CLR_N = "\x1b[0m"
+)
---
+++ color_windows.go
@@ -0,0 +1,16 @@
+// +build windows
+
+package loglevel
+
+// Colored output is currently not supported by this package
+const (
+	CLR_0 = ""
+	CLR_R = ""
+	CLR_G = ""
+	CLR_Y = ""
+	CLR_B = ""
+	CLR_M = ""
+	CLR_C = ""
+	CLR_W = ""
+	CLR_N = ""
+)
